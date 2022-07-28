d0ebfc3c3b2fd2a5e10d7358c31f9883e2c7dec3
Saravanan Ramanathan <srvnnramanathan@gmail.com>
2022-07-28 08:43:03 +0530

    created model for budgets
    --full budget model creating
      --3 different models: budget, shares, items linked together.
    --registered with inline

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
-
+from .import models
 # Register your models here.
+
+
+class sharedView(admin.StackedInline):
+    model = models.share
+
+
+class ItemView(admin.StackedInline):
+    model = models.Item
+
+
+@admin.register(models.Budget)
+class budget(admin.ModelAdmin):
+    inlines = [ItemView, sharedView]
+
+    class Meta:
+        model = models.Budget
+
+
+"""
+#if needed to see them seperate...
+@admin.register(models.share)
+class sharedView(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Item)
+class ItemView(admin.ModelAdmin):
+    pass
+"""
+
+# admin.site.register(models.Budget)
+# admin.site.register(models.Item)
+# admin.site.register(models.Shared)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,59 @@
+"""
+database modles
+"""
 from django.db import models
+from userAccess.models import CustomUser as User
 
-# Create your models here.
+
+class Budget(models.Model):
+    "database object called ToDoList"
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="budget", null=False)
+    # shared_with = models.ForeignKey(
+    # User, on_delete=models.CASCADE, related_name="shared_with", null=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class share(models.Model):
+    "details of shared budgets"
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    shared_with = models.OneToOneField(
+        User, on_delete=models.SET_NULL, null=True)
+
+
+class Accounts(models.IntegerChoices):
+    INCOME = 1, "Income"
+    EXPENSE = 2, "Expense"
+
+
+class Item(models.Model):
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    itemName = models.CharField(max_length=300)
+    type = models.IntegerField(choices=Accounts.choices)
+    income = models.IntegerField(null=True, blank=True)
+    expense = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.itemName)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name="selecting_income_or_expense",
+                check=(
+                    models.Q(
+                        type=Accounts.EXPENSE,
+                        income__isnull=True,
+                        expense__isnull=False,
+                    ) |
+                    models.Q(
+                        type=Accounts.INCOME,
+                        income__isnull=False,
+                        expense__isnull=True,
+                    )
+                ),
+            )
+        ]
